8c438b28e67534539e7eb4bf02ce9b80c5a0b79f
Mr-Smilin <lion31lion31@gmail.com>
2026-01-07 16:43:15 +0800

    Updates

---
+++ assets/app-7xUnKn0N.js
@@ -0,0 +1,42 @@
+import{a as e,n as t,o as n,t as r}from"./app-BBji5VRM.js";var i=t(),a=e(),o=`/assets/react-CHdo91hT.svg`,s=`/vite.svg`,c=`#root {\r
+	max-width: 1280px;\r
+	margin: 0 auto;\r
+	padding: 2rem;\r
+	text-align: center;\r
+}\r
+\r
+.logo {\r
+    height: 6em;\r
+    padding: 1.5em;\r
+    will-change: filter;\r
+    transition: filter 300ms;\r
+}\r
+.logo:hover {\r
+    filter: drop-shadow(0 0 2em #646cffaa);\r
+}\r
+.logo.react:hover {\r
+    filter: drop-shadow(0 0 2em #61dafbaa);\r
+}\r
+\r
+@keyframes logo-spin {\r
+    from {\r
+        transform: rotate(0deg);\r
+    }\r
+    to {\r
+        transform: rotate(360deg);\r
+    }\r
+}\r
+\r
+@media (prefers-reduced-motion: no-preference) {\r
+    a:nth-of-type(2) .logo {\r
+        animation: logo-spin infinite 20s linear;\r
+    }\r
+}\r
+\r
+.card {\r
+    padding: 2em;\r
+}\r
+\r
+.read-the-docs {\r
+    color: #888;\r
+}`,l=n(),u=r`${c}`;function d(){let e=(0,i.c)(13),[t,n]=(0,a.useState)(0),r;e[0]===Symbol.for(`react.memo_cache_sentinel`)?(r=(0,l.jsx)(u,{}),e[0]=r):r=e[0];let c;e[1]===Symbol.for(`react.memo_cache_sentinel`)?(c=(0,l.jsx)(`a`,{href:`https://vite.dev`,target:`_blank`,children:(0,l.jsx)(`img`,{src:s,className:`logo`,alt:`Vite logo`})}),e[1]=c):c=e[1];let d,p;e[2]===Symbol.for(`react.memo_cache_sentinel`)?(d=(0,l.jsxs)(`div`,{children:[c,(0,l.jsx)(`a`,{href:`https://react.dev`,target:`_blank`,children:(0,l.jsx)(`img`,{src:o,className:`logo react`,alt:`React logo`})})]}),p=(0,l.jsx)(`h1`,{children:`學習 phaser `}),e[2]=d,e[3]=p):(d=e[2],p=e[3]);let m;e[4]===Symbol.for(`react.memo_cache_sentinel`)?(m=()=>n(f),e[4]=m):m=e[4];let h;e[5]===t?h=e[6]:(h=(0,l.jsxs)(`button`,{onClick:m,children:[`count is `,t]}),e[5]=t,e[6]=h);let g;e[7]===Symbol.for(`react.memo_cache_sentinel`)?(g=(0,l.jsxs)(`p`,{children:[`Edit `,(0,l.jsx)(`code`,{children:`src/App.jsx`}),` and save to test HMR`]}),e[7]=g):g=e[7];let _;e[8]===h?_=e[9]:(_=(0,l.jsxs)(`div`,{className:`card`,children:[h,g]}),e[8]=h,e[9]=_);let v;e[10]===Symbol.for(`react.memo_cache_sentinel`)?(v=(0,l.jsx)(`p`,{className:`read-the-docs`,children:`Click on the Vite and React logos to learn more`}),e[10]=v):v=e[10];let y;return e[11]===_?y=e[12]:(y=(0,l.jsxs)(l.Fragment,{children:[r,d,p,_,v]}),e[11]=_,e[12]=y),y}function f(e){return e+1}var p=d;export{p as default};
